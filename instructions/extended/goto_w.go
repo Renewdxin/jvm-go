74b5ccbb5e5f84af9ae85e65f75eccaabe5ce6eb
“RenXin11111” <cissyrenxin@icloud.com>
2024-06-10 19:26:16 +0800

    扩展指令、解释器

--- instructions/extended/goto_w.go
+++ instructions/extended/goto_w.go
@@ -11,6 +11,7 @@ type GOTO_W struct {
 	offset int
 }
 
+// goto_w指令和goto指令的唯一区别就是索引从2字节变成了4字节
 func (gt *GOTO_W) FetchOperands(reader *base.BytecodeReader) {
 	gt.offset = int(reader.ReadInt32())
 }
